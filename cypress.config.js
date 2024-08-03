e6c2260164950825889bc8ad353095478e36ec58
fernanda antonaccio <fernanda.antonaccio@gmail.com>
2024-08-03 10:52:16 -0400

    Ajuste

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://serverest.dev',
-    video:true,
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  video:true
 });
